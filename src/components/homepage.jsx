c23835026214f8240c49a3c66cc6b1a9c7f44c99
kakashi8963 <mananbafna00@gmail.com>
2021-04-17 14:12:20 +0530

    done

---
+++ src/components/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/home.styles.css';
+
+const Homepage = () =>{
+    return(
+<div>
+<h1 className='header'> X-SELL </h1>
+
+</div>
+    )
+};
+
+export default Homepage;
